b0576582c63590a8d4f200df542a779827d69e5c
Chuck Flowers <Chuck Flowers>
2017-01-22 14:08:21 -0500

    Creates a synchronous implementation of the IntelCamera class

---
+++ Senior Design/Assets/plugins/CameraIO/ColorDepthImage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnityScanner3D.CameraIO
+{
+    public struct ColorDepthImage
+    {
+        public int[] ColorImage;
+        public int[] DepthImage;
+    }
+}
