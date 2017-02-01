dd3814c63522a15f4f0c3841422f48822ce37cec
Mark McCulloh <Mark McCulloh>
2017-01-31 19:47:12 -0500

    Set up Unity Project with everything (hopefully) included

---
+++ 3DScanner/Assets/Plugins/CameraIO/ColorDepthImage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace UnityScanner3D.CameraIO
+{
+    public struct ColorDepthImage
+    {
+        public Texture2D ColorImage;
+        public Texture2D DepthImage;
+    }
+}
---
+++ 3DScanner/Assets/Plugins/CameraIO/IntelCamera.cs
@@ -0,0 +1,76 @@
+﻿using Intel.RealSense;
+using System;
+
+namespace UnityScanner3D.CameraIO
+{
+    public class IntelCamera : ICamera
+    {
+        ~IntelCamera()
+        {
+            if(SampleStream != null)
+                SampleStream.Dispose();
+            if(SMInstance != null)
+                SMInstance.Dispose();
+        }
+
+        public ColorDepthImage GetImage()
+        {
+            if (Status == CameraStatus.Stopped)
+                throw new InvalidOperationException("The camera cannot produce data unless it is running");
+
+            if (SMInstance.AcquireFrame(true).IsError())
+                throw new Exception("Unable to captuer frame");
+
+            ColorDepthImage toRet = new ColorDepthImage();
+
+            //Extract the color image
+            Image colorImage = SampleStream.Sample.Color;
+            ImageData colorImageData;
+            colorImage.AcquireAccess(ImageAccess.ACCESS_READ, out colorImageData);
+            colorImageData.ToTexture2D(0, toRet.ColorImage);
+
+            //Extract the depth image
+            Image depthImage = SampleStream.Sample.Depth;
+            ImageData depthImageData;
+            depthImage.AcquireAccess(ImageAccess.ACCESS_READ, out depthImageData);
+            depthImageData.ToTexture2D(0, toRet.DepthImage);
+
+            //Clean up image data
+            colorImage.Dispose();
+            depthImage.Dispose();
+    
+            return toRet;
+        }
+
+        public void StartCapture()
+        {
+            if (Status != CameraStatus.Stopped)
+                throw new InvalidOperationException("A camera cannot be started if it is already running");
+
+            Status = CameraStatus.Running;
+
+            //Initialize the sense manager and streams
+            SampleStream = SampleReader.Activate(SMInstance);
+            SampleStream.EnableStream(StreamType.STREAM_TYPE_COLOR);
+            SampleStream.EnableStream(StreamType.STREAM_TYPE_DEPTH);
+            SMInstance.Init();
+        }
+
+        public void StopCapture()
+        {
+            //Updates the status
+            Status = CameraStatus.Stopped;
+
+            //Dispose of the created stream
+            SampleStream.Dispose();
+            SampleStream = null;
+
+            //Close the sense manager
+            SMInstance.Close();
+        }
+
+        private SenseManager SMInstance = SenseManager.CreateInstance();
+        private SampleReader SampleStream;
+        private CameraStatus Status = CameraStatus.Stopped;
+    }
+}
