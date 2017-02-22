0e42f78e9d221eb57030f8a91d73baa615ecdd61
Timothy Flowers <t_chuck_flowers@yahoo.com>
2017-02-22 15:21:31 -0500

    Adds an IAlgorithm class to define common interface for CV algorithms and adds a Shape struct to store a shape's type and 6 dimensional pose info

---
+++ 3DScanner/Assets/Plugins/ComputerVision/IAlgorithm.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityScanner3D.CameraIO;
+
+namespace UnityScanner3D.ComputerVision
+{
+    interface IAlgorithm
+    {
+        void ProcessImage(ColorDepthImage image);
+        IEnumerable<Shape> GetShapes();
+    }
+}
---
+++ 3DScanner/Assets/Plugins/ComputerVision/Shape.cs
@@ -0,0 +1,52 @@
+﻿namespace UnityScanner3D.ComputerVision
+{
+    /// <summary>
+    /// Defines the possible shape types
+    /// </summary>
+    enum ShapeType
+    {
+        Cube,
+        Cylinder
+    }
+
+    /// <summary>
+    /// A struct for describing a shaped detected by an <see cref="IAlgorithm"/>
+    /// </summary>
+    struct Shape
+    {
+        /// <summary>
+        /// Specifies the type of the shape (e.g. Cube, Arch, Cylinder, etc.)
+        /// </summary>
+        public ShapeType Type { get; set; }
+
+        /// <summary>
+        /// X axis position (Translational)
+        /// </summary>
+        public double X { get; set; }
+
+        /// <summary>
+        /// Y axis position (Translational)
+        /// </summary>
+        public double Y { get; set; }
+
+        /// <summary>
+        /// Z axis position (Translational)
+        /// </summary>
+        public double Z { get; set; }
+
+        /// <summary>
+        /// U position (Rotational)
+        /// </summary>
+        public double U { get; set; }
+
+        /// <summary>
+        /// V position (Rotational)
+        /// </summary>
+        public double V { get; set; }
+
+        /// <summary>
+        /// W position (Rotational)
+        /// </summary>
+        public double W { get; set; }
+    }
+}
