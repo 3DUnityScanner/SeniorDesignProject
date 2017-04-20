4cde8aa4aeabbc054dd0b690a945b328df3c6edf
Timothy Flowers <t_chuck_flowers@yahoo.com>
2017-04-20 18:39:30 -0400

    Adds clump type

---
+++ 3DScanner/Assets/Plugins/ComputerVision/Clump.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace UnityScanner3D.ComputerVision
+{
+    class Clump
+    {
+        public Clump(IEnumerable<Vector3> points)
+        {
+            Points = points;
+        }
+
+        public Color Color { get; set; }
+        public List<Vector3> Points { get; private set; }
+
+        public IEnumerable<Color> GetClumpPixels(Texture2D image)
+        {
+            foreach (var p in Points)
+                yield return image.GetPixel(p.x, p.y);
+        }
+    }
+}
